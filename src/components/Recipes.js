c0d75ec0c4354b8266bd898e6d0ab67fbc31b3b4
Alexey Zvezdin <alexeyrush94@gmail.com>
2019-06-07 16:01:35 +0400

    Adding react router

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -4,17 +4,29 @@ const Recipes = props => (
   <div className="container">
     <div className="row">
       {props.recipes.map(recipe => (
-        <div className="col-md-4">
-          <div className="recipe__box">
+        <div
+          key={recipe.title}
+          className="col-md-4"
+          style={{ marginBottom: "2rem" }}
+        >
+          <div className="recipes__box">
             <img
-              className="recipe__box-img"
+              className="recipes__box-img"
               src={recipe.image_url}
               alt={recipe.title}
             />
 
             <div className="recipe__text">
-              <h5>{recipe.title}</h5>
+              <h5 className="recipes__title">
+                {recipe.title.length < 20
+                  ? `${recipe.title}`
+                  : `${recipe.title.substring(0, 25)}...`}
+              </h5>
+              <p className="recipes__title">
+                Publisher <span>{recipe.publisher}</span>
+              </p>
             </div>
+            <button class="recipe_buttons">View Recipe</button>
           </div>
         </div>
       ))}
